a343a36c4ca09d18b878dc64471abcd7b383ffda
allenwang430 <64767262+allenwang430@users.noreply.github.com>
2021-01-25 11:33:14 +0800

    update

---
+++ docs/precache-manifest.b1f57b1fb25b5694be43eb7a0c09be36.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d760602dc283e0938b",
+    "url": "/public/assets/css/about.css"
+  },
+  {
+    "revision": "5ba071aef5cef1e012ca",
+    "url": "/public/assets/css/app.css"
+  },
+  {
+    "revision": "37f1d0ec1865e048189c",
+    "url": "/public/assets/css/chunk-14fc1619.css"
+  },
+  {
+    "revision": "e327c5c4f2746a058902",
+    "url": "/public/assets/css/chunk-5453ccfb.css"
+  },
+  {
+    "revision": "61c76bb821c192bd3c62",
+    "url": "/public/assets/css/chunk-a8a06640.css"
+  },
+  {
+    "revision": "36cb3fb9aca3f2b107fe",
+    "url": "/public/assets/css/randomuser.css"
+  },
+  {
+    "revision": "281891bab755a77e5fb1",
+    "url": "/public/assets/css/tab01.css"
+  },
+  {
+    "revision": "f5aabbe5b47207017e5f",
+    "url": "/public/assets/css/tab02.css"
+  },
+  {
+    "revision": "98768cca14a088ced977",
+    "url": "/public/assets/css/vue01.css"
+  },
+  {
+    "revision": "f90d754384150a006536",
+    "url": "/public/assets/css/vue02.css"
+  },
+  {
+    "revision": "d5d760602dc283e0938b",
+    "url": "/public/assets/js/about.js"
+  },
+  {
+    "revision": "5ba071aef5cef1e012ca",
+    "url": "/public/assets/js/app.js"
+  },
+  {
+    "revision": "3a45c42777b39b8faf98",
+    "url": "/public/assets/js/carousel.js"
+  },
+  {
+    "revision": "37f1d0ec1865e048189c",
+    "url": "/public/assets/js/chunk-14fc1619.js"
+  },
+  {
+    "revision": "e327c5c4f2746a058902",
+    "url": "/public/assets/js/chunk-5453ccfb.js"
+  },
+  {
+    "revision": "61c76bb821c192bd3c62",
+    "url": "/public/assets/js/chunk-a8a06640.js"
+  },
+  {
+    "revision": "0d55906d918edb4b685c",
+    "url": "/public/assets/js/chunk-vendors.js"
+  },
+  {
+    "revision": "0aca486fc38b6a69548f0296b0802a8f",
+    "url": "/public/assets/js/main.js"
+  },
+  {
+    "revision": "183b2025725c7335f828fa97f8c777fc",
+    "url": "/public/assets/js/myapp.js"
+  },
+  {
+    "revision": "36cb3fb9aca3f2b107fe",
+    "url": "/public/assets/js/randomuser.js"
+  },
+  {
+    "revision": "263ebf17e578a51db22a1120a13de0ef",
+    "url": "/public/assets/js/skipButton.js"
+  },
+  {
+    "revision": "281891bab755a77e5fb1",
+    "url": "/public/assets/js/tab01.js"
+  },
+  {
+    "revision": "f5aabbe5b47207017e5f",
+    "url": "/public/assets/js/tab02.js"
+  },
+  {
+    "revision": "98768cca14a088ced977",
+    "url": "/public/assets/js/vue01.js"
+  },
+  {
+    "revision": "f90d754384150a006536",
+    "url": "/public/assets/js/vue02.js"
+  },
+  {
+    "revision": "68686acc1e19a8974520e65d6f9a75cd",
+    "url": "/public/data/AgqYcfAT-FctPAkow.mp4"
+  },
+  {
+    "revision": "a7d7818714a459e17daa8ed26f740774",
+    "url": "/public/data/example_01.json"
+  },
+  {
+    "revision": "6ecb2433d99b0f2209a4b7cbfeb5a365",
+    "url": "/public/data/example_02.json"
+  },
+  {
+    "revision": "27ed956c25606bb6fe666479901374c4",
+    "url": "/public/data/example_03.json"
+  },
+  {
+    "revision": "be079066b17d55a4faf1e13cadd057c1",
+    "url": "/public/data/example_10.json"
+  },
+  {
+    "revision": "f95be6d02113d8dbb8d289488e576a0a",
+    "url": "/public/data/home.json"
+  },
+  {
+    "revision": "e41caa41478df2e0b97606a1591bb30e",
+    "url": "/public/data/sliderInfo.json"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/public/data/tab.json"
+  },
+  {
+    "revision": "8601f8d02de4931e8b090db96a55e17e",
+    "url": "/public/img/download.svg"
+  },
+  {
+    "revision": "9768fcb2e1531086cee180880eacbcf0",
+    "url": "/public/img/picture/bg-001.jpg"
+  },
+  {
+    "revision": "4135d13e0ec7e6ee89055cd9a031dca4",
+    "url": "/public/img/picture/grid-001.jpg"
+  },
+  {
+    "revision": "7c3d1a58ede78bb1c4bd41b3052eb4ac",
+    "url": "/public/img/picture/grid-002.jpg"
+  },
+  {
+    "revision": "5b5751a8dd636c0964bbe3d71364c6ad",
+    "url": "/public/img/picture/grid-003.jpg"
+  },
+  {
+    "revision": "909dcb9c034b5ad2160fdb7655f4b034",
+    "url": "/public/img/picture/photo-001.jpg"
+  },
+  {
+    "revision": "59686f1ecc32cb6b08f222af5cc76782",
+    "url": "/public/img/picture/photo-002.jpg"
+  },
+  {
+    "revision": "4a51f63c4e56158862a11553dfee4ed5",
+    "url": "/public/img/picture/photo-003.jpg"
+  },
+  {
+    "revision": "9991f5a3eba8a4c0ea2e004d00df9ca2",
+    "url": "/public/index.html"
+  },
+  {
+    "revision": "e748e66cfb438525e92ea43a7fde08e1",
+    "url": "/public/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/public/robots.txt"
+  },
+  {
+    "revision": "34a329dd71ff7f9f29f4583784cf50f2",
+    "url": "/public/tracks/track.vtt"
+  }
+]);
