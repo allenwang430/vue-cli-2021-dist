17602d32ed7e9d3e7a56ed0f502a5bc55e023998
allenwang430 <64767262+allenwang430@users.noreply.github.com>
2021-01-25 10:50:12 +0800

    update

---
+++ precache-manifest.657bc28a3718b66addb4cd4f372aa512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077a41dda3a96dda7a19",
+    "url": "assets/css/app.css"
+  },
+  {
+    "revision": "f8f74bbb607725edba3c",
+    "url": "assets/css/chunk-vendors.css"
+  },
+  {
+    "revision": "077a41dda3a96dda7a19",
+    "url": "assets/js/app.js"
+  },
+  {
+    "revision": "f8f74bbb607725edba3c",
+    "url": "assets/js/chunk-vendors.js"
+  },
+  {
+    "revision": "fe8ec05d4856817bdef3d533302c76d4",
+    "url": "index.html"
+  },
+  {
+    "revision": "79e97efcaa1bf55ee41e0e773c0e8663",
+    "url": "manifest.json"
+  }
+]);
