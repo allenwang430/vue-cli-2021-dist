74d4b1cc7802c55c8dc0c531e51e4b8a03453438
allenwang430 <64767262+allenwang430@users.noreply.github.com>
2021-01-28 12:18:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello World! This is simple express server')
 })
 
-// start a server on port 9090
-const server = app.listen(9090, () => {
+// start a server on port 50000
+const server = app.listen(50000, () => {
   const port = server.address().port
-  console.log('Example app listening on port ----- 9090', port)
+  console.log('Example app listening on port ----- 50000', port)
 })
