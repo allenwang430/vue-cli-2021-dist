bd4174f42d1a9937de2606c5e8867a9cfb12b480
allenwang430 <64767262+allenwang430@users.noreply.github.com>
2021-02-05 16:26:29 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,13 +2,20 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      Kyart 2021/2/5 開始作業 , pm 16:05 , For a guide and recipes on how to
+      Kyart 2021/2/5 開始作業 , pm 16:26 , For a guide and recipes on how to
       configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
       >.
     </p>
+    <h3>CI / CD 工具</h3>
+    <ul>
+      <li>jenkins</li>
+      <li>Issue Tracking System</li>
+      <li>GitLab</li>
+      <li>Drone</li>
+    </ul>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li>
